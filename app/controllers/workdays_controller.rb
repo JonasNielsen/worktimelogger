2b16a274175c9acbe98fc19d986e3c495d0582dd
Jonas Hansen <kyllesdk@gmail.com>
2013-10-23 17:33:12 +0200

    Added cookie support

--- app/controllers/workdays_controller.rb
+++ app/controllers/workdays_controller.rb
@@ -1,27 +1,55 @@
 class WorkdaysController < ApplicationController
 
 	def index
+		time = Time.new
+		dateStart = time.strftime('01/%m/%Y').to_date
+		dateEnd = time.strftime('31/%m/%Y').to_date
+
 		@user = User.find(session[:user_id])
-		@workdays = @user.workdays
+		@workdays = @user.workdays.find(:all, :conditions => ['date >= ? and date <= ?', dateStart, dateEnd])
+	end
+
+	def new
+		time = Time.new
+		date = time.strftime('%d/%m/%Y').to_date
+		@user = User.find(session[:user_id])
+		@workday = @user.workdays.where(:date => date).first
 	end
 
 	def create
+		@user = User.find(session[:user_id])
 		attrs = params[:workday]
-		workday = Workday.new()
 
-		workday.worktime = attrs[:worktime]
-		workday.date = attrs[:date]
-		workday.description = attrs[:description]
-		workday.user_id = session[:user_id]
+		workday = @user.workdays.where(:date => attrs[:date].to_date).first
+		if workday != nil
+			workday.worktime = attrs[:worktime]
+			workday.description = attrs[:description]
+		else
+			workday = Workday.new()
+
+			workday.worktime = attrs[:worktime]
+			workday.date = attrs[:date].to_date
+			workday.description = attrs[:description]
+			workday.user_id = session[:user_id]
+		end
 
 		if workday.save
 			flash[:successMsg] = "Tiden er nu gemt."
 		end
 	end
 
+	def storeTimeCookie
+		cookies[:timeJson] = params[:time]
+	end
+
+	def loadTimeCookie
+		time = cookies[:timeJson]
+		time #return json of time
+	end
+
 	def filter
 		@user = User.find(session[:user_id])
-		@workdays = @user.workdays.find(:all, :conditions => ['date > ? AND date < ?', params[:fromDate], params[:toDate]])
+		@workdays = @user.workdays.find(:all, :conditions => ['date >= ? AND date <= ?', params[:fromDate].to_s.to_date, params[:toDate].to_s.to_date])
 
 		render 'index'
 	end
