b77bb7743e4221be7fccc565b02229b03aed7987
Jonas Hansen <kyllesdk@gmail.com>
2013-10-23 23:42:38 +0200

    Created seeds for workdays

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+#USER
+
+## Workdays
+Workday.create(:worktime => '2.5', :date => '01/10/2013'.to_date, :description => 'Uha alt det jeg har lavet idag!', :user_id => '1')
+Workday.create(:worktime => '3', :date => '02/10/2013'.to_date, :description => 'Lavet meget mere!', :user_id => '1')
+Workday.create(:worktime => '2', :date => '03/10/2013'.to_date, :description => 'Endnu mere!', :user_id => '1')
+Workday.create(:worktime => '0.25', :date => '05/10/2013'.to_date, :description => 'Orker ikke mere!', :user_id => '1')
+Workday.create(:worktime => '1', :date => '07/10/2013'.to_date, :description => 'Uha alt det jeg har lavet idag!', :user_id => '1')
+Workday.create(:worktime => '6', :date => '08/10/2013'.to_date, :description => 'Uha alt det jeg har lavet idag!', :user_id => '1')
+Workday.create(:worktime => '7.5', :date => '10/10/2013'.to_date, :description => 'Uha alt det jeg har lavet idag!', :user_id => '1')
+Workday.create(:worktime => '3.75', :date => '13/10/2013'.to_date, :description => 'Uha alt det jeg har lavet idag!', :user_id => '1')
+Workday.create(:worktime => '2.5', :date => '15/10/2013'.to_date, :description => 'Uha alt det jeg har lavet idag!', :user_id => '1')
+Workday.create(:worktime => '1', :date => '16/10/2013'.to_date, :description => 'Uha alt det jeg har lavet idag!', :user_id => '1')
+Workday.create(:worktime => '2', :date => '20/10/2013'.to_date, :description => 'Uha alt det jeg har lavet idag!', :user_id => '1')
+Workday.create(:worktime => '5.5', :date => '21/10/2013'.to_date, :description => 'Uha alt det jeg har lavet idag!', :user_id => '1')
+Workday.create(:worktime => '6.25', :date => '24/10/2013'.to_date, :description => 'Uha alt det jeg har lavet idag!', :user_id => '1')
+Workday.create(:worktime => '0.75', :date => '25/10/2013'.to_date, :description => 'Uha alt det jeg har lavet idag!', :user_id => '1')
+Workday.create(:worktime => '0.5', :date => '26/10/2013'.to_date, :description => 'Uha alt det jeg har lavet idag!', :user_id => '1')
+Workday.create(:worktime => '2.5', :date => '28/10/2013'.to_date, :description => 'Uha alt det jeg har lavet idag!', :user_id => '1')
+Workday.create(:worktime => '1', :date => '29/10/2013'.to_date, :description => 'Uha alt det jeg har lavet idag!', :user_id => '1')
+Workday.create(:worktime => '0.5', :date => '30/10/2013'.to_date, :description => 'Uha alt det jeg har lavet idag!', :user_id => '1')
+
+
